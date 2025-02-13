7ef18744a7c86cadc671017d1124efa9953273ef
HSH-85 <gjtjdgns@gmail.com>
2025-02-13 10:42:40 +0900

    forLoop

--- js/10.js
+++ js/10.js
@@ -7,4 +7,50 @@ $(document).ready(function(){
     { name : '이서', age : 18 },
     { name : '가을', age : 23 }
   ]
+
+  $("#btn_forLoop").on("click", function(){
+    let result = ''
+    //for 구문
+    for(let i =0;i<data.length;i++){
+      result=result + `
+         <tr>
+            <td>${i+1}</td>
+            <td>${data[i].name}</td>
+            <td>${data[i].age}</td>
+          </tr>
+          `
+    }
+    $("#forOut").html(result)
+  })
+
+  $("#btn_forEach").on("click", function(){
+    let result=''
+    // foreach 구문 foreach 안쓰고 Map써도됨
+    data.forEach((x, index)=>{
+      result=result + `
+         <tr>
+            <td>${index+1}</td>
+            <td>${x.name}</td>
+            <td>${x.age}</td>
+          </tr>
+          `
+    })
+    $("#forEach").html(result)
+
+  })
+
+    $("#btn_jqueryEach").on("click", function(){
+      let result=''
+      $.each(data,(index, x)=>{
+        result=result + `
+           <tr>
+              <td>${index+1}</td>
+              <td>${x.name}</td>
+              <td>${x.age}</td>
+            </tr>
+            `
+      })
+      $("#jqueryEach").html(result)
+    })
+  
 })
